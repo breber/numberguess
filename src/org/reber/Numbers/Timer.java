b8b37d35190a5f7f3d36e10929cefa7e16634042
reber.brian <reber.brian@32d5bd8f-86a9-3f13-ef56-eeefe2645057>
2010-04-05 18:12:39 +0000

    Javadoc'd the Timer class
    git-svn-id: http://android-number-guess.googlecode.com/svn/trunk/Numbers@11 32d5bd8f-86a9-3f13-ef56-eeefe2645057

--- src/org/reber/Numbers/Timer.java
+++ src/org/reber/Numbers/Timer.java
@@ -1,8 +1,9 @@
 package org.reber.Numbers;
 
 /**
+ * A simple stopwatch-like timer.
+ * 
  * @author brianreber
- *
  */
 public class Timer {
 
@@ -10,6 +11,9 @@ public class Timer {
 	private long endTime;
 	private boolean stopped;
 	
+	/**
+	 * Create a new Timer instance that is stopped.
+	 */
 	public Timer()
 	{
 		stopped = true;
@@ -17,16 +21,29 @@ public class Timer {
 		endTime = -1;
 	}
 
+	/**
+	 * Start timing.
+	 */
 	public void start() {
 		startTime = System.currentTimeMillis();
 		stopped = false;
 	}
 
+	/**
+	 * Stop timing.
+	 */
 	public void stop() {
 		endTime = System.currentTimeMillis();
 		stopped = true;
 	}
 
+	/**
+	 * Get amount of seconds ellapsed.
+	 * 
+	 * @return
+	 * Amount of seconds ellapsed from starting time.<br />
+	 * (-1) if the timer hasn't been started 
+	 */
 	public double getSeconds() {
 		if (stopped && endTime != -1)
 			return ((endTime - startTime) / 1000.0);
